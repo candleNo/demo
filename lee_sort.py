52a8dcc4a4d161d25e17b527a484363c7e68e306
aipus <13242858414@163.com>
2019-01-20 11:05:33 -0500

    bug

--- lee_sort.py
+++ lee_sort.py
@@ -1,2 +1,22 @@
+import re
 def lee_sort(arr):
-  
+  newArr = []
+  index = 0
+
+  isList = re.search("list", str(type(arr)))
+  if isList is not None:
+    while index < len(arr):
+      max = 0
+      for item in range(index, len(arr)-1):
+        if(arr[item] < arr[item + 1]):
+          max = arr[item + 1]
+        else:
+          max = arr[item]
+      newArr.append(max)
+      index = index + 1
+      print(index)
+  return newArr
+
+arr = [1,9,8,12,6] 
+
+print(lee_sort(arr))
